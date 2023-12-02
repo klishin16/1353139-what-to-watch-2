708f55842da8c4382f7cd41828b0370a0469598d
Nikita Klishin <n.klishin@oblakogroup.ru>
2023-12-02 14:41:49 +0300

    Added module 5 task 1

---
+++ src/components/video-player/video-player.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from 'react';
+
+interface IVideoPlayerProps {
+  src: string;
+  poster: string;
+  muted: boolean;
+  playPreview: boolean;
+  height: number;
+  width: number;
+}
+
+const VideoPlayer = ({ src, poster, muted, playPreview, height, width }: IVideoPlayerProps) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (playPreview) {
+      videoRef.current?.play();
+    } else {
+      videoRef.current?.load();
+    }
+  }, [playPreview]);
+
+  return (
+    <video ref={videoRef} src={src} poster={poster} muted={muted} height={height} width={width} autoPlay={false}></video>
+  );
+};
+
+export default VideoPlayer;
